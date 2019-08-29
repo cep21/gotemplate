cd83911e9480a73bb7600c26d4d7d961e3201e11
Jack Lindamood <cep221@gmail.com>
2019-08-29 10:24:45 -0700

    tools: document tools.go reason in file

--- tools.go
+++ tools.go
@@ -2,4 +2,6 @@
 
 package gotemplate
 
+// External tools.  See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
 import _ "github.com/golangci/golangci-lint/cmd/golangci-lint"
